c5bbc68599d3f2d6e7cd079cc054f9b0895b033a
Naman Muley <naman.g.muley@gmail.com>
2025-07-29 19:12:38 +0530

    Add alembic/versions/0001_create_users_table.py

---
+++ alembic/versions/0001_create_users_table.py
@@ -0,0 +1,12 @@
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), primary_key=True),
+        sa.Column('email', sa.String(), nullable=False, unique=True, index=True),
+    )
+
+def downgrade():
+    op.drop_table('users')
